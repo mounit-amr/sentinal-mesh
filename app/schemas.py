a95ad7501ddea5afbc9942e6ba9a1dd7950a2d4d
Mounit <mounitmankotia21@gmail.com>
2026-06-26 23:58:56 +0530

    red

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from sqlalchemy import column, Integer, String, DateTime
-from datetime import datetime
+from datetime import datetime#dont know why redlines
 
 class user(Base):
     __tablename__ = "user"
@@ -11,3 +11,4 @@ class user(Base):
     role = column(String) 
     
     email = column(String, unique = True)
+    
