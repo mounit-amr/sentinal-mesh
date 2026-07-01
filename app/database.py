9121cbad9a6267967840a1de20d553a365235e2c
Mounit <mounitmankotia21@gmail.com>
2026-07-01 13:42:53 +0530

    databseupdate

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 
 DATABASE_URL = ""
 
@@ -14,3 +14,11 @@ Sessionlocal = sessionmaker(
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
